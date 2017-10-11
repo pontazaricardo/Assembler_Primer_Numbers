68ba20ba6bc404d7823437085439f07998cc98b7
pontazaricardo <pontaza.ricardo@gmail.com>
2017-10-11 10:31:47 +0800

    Update

--- main/determine_primes.asm
+++ main/determine_primes.asm
@@ -2,12 +2,18 @@
 # Messages to be displayed to the user
 
 Message: .asciiz  "Please enter a number: "
-number1:   .word   0
+number1: .word 0
 
 .text
 _Main:
 
 	# Ask to the user to enter the number to test
-	la      $a0, Message
-        li      $v0, 4
-        syscall
+	la	$a0, Message
+	li	$v0, 4
+	syscall
+
+	# Get the integer and store it in $t0
+	li	$v0, 5
+	syscall 
+	sw	$v0, number1 #Store the number in $v0
+	lw	$t0, number1 #Load the number in $t0
